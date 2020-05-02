69102151c4ca46c04fb32e85f51de59454152a50
sumitkumar joshi <ssquare489@gmail.com>
2020-05-02 12:51:28 +0530

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
+        <p>World Map With Covid-19 data</p>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/page-2/">Page 2</Link>
+            <Link to="/page-2/"></Link>
           </li>
         </ul>
       </Container>
--- src/data/map-services.js
+++ src/data/map-services.js
@@ -3,5 +3,10 @@ export const mapServices = [
     name: 'OpenStreetMap',
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  },
+  {
+    name: 'Mapbox',
+    attribution: '',
+    url: 'https://api.mapbox.com/styles/v1/sumitjoshi489/ck9p8m4mp1u1g1js0w9oriz5g/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3VtaXRqb3NoaTQ4OSIsImEiOiJjazlwOHFqajMwM3BtM2ZvMjNoNnN6cHJ2In0.0ty1-BfZme7w5m2eNwpIhQ'
   }
 ];
